29376d393580b79d6035286d3c64181dc680f33b
Manoj <manoj.vattarai@gmail.com>
2022-03-22 08:37:01 +0545

    url to re_path

--- xmldemo/urls.py
+++ xmldemo/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
@@ -8,6 +8,6 @@ from django.views.generic.base import RedirectView, TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('home.urls')),
-    url(r'^(?!/?static/)(?P<path>.*\..*)$',
+    re_path(r'^(?!/?static/)(?P<path>.*\..*)$',
         RedirectView.as_view(url='/static/%(path)s', permanent=False)),
 ]
